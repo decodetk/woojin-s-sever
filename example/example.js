dc07afc33a16dcf0812d807d745b5f1096077fbb
unknown <woojin961215@gmail.com>
2022-04-20 17:54:32 +0900

    4월 20일

---
+++ example/example.js
@@ -0,0 +1,51 @@
+
+// let one = "1";
+// // 변수를 선언한다 이름은 one 이다, 선언한 김에 값도 대입한다, 그 값의 숫자타입의 1이다
+// let two = 1;
+
+// console.log(one + two);
+// console.log(one);
+// // 자바스크립트 변수 one이 뭔지 모르겠는데, 실행창에 기록 좀 남겨달라
+// console.log(typeof one);
+// // 자스야 변수 one 이 데이터 타입이 뭔지 모르겠다
+
+// let father = "아버지가";
+// let room = "방에 들어가시다";
+
+// console.log(typeof (father + room));
+// // 자스야 father라는 변수와 room 이라는 변수를 더한다음 데이터 타입좀 체크 해 줄래?
+
+// let na = "공욱재는 미남이다.";
+// if(typeof na === "string") {
+//   // 만약에 자스야, 소괄호에 있는 내용이 맞으면 중괄호에 있는 것좀 실행 시켜줘
+//   console.log("아하하하하하하하하");
+// }
+
+// let sohi = {
+//   name: "sohi" ,
+//   gender: "female"
+// }
+// console.log(typeof sohi);
+// if(sohi.name === "ukjae") {
+//   console.log("미남이야");
+// } else {
+//   console.log("틀렸어 임마");
+// }
+
+// let a = 1;
+// let b = 2;
+// let result = "값이 다릅니다."
+// if(a === b) {
+//   console.log('값이 같습니다.');
+// } else {
+//   console.log(result);
+//   // 거짓인 경우 변수인 result를 기록하는 행동만 할 뿐이다 변수의 값이 무엇인지는 관심이 없다.
+// }
+
+let a = 1;
+let b = 2;
+if(a !== b) {
+  console.log('값이 같습니다')
+} else {
+  console.log('값이 다릅니다')
+}
